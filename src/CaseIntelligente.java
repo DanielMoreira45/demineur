5176e5c17e3b5ef71964471e6445a536e2151496
daniel_moreira <daniel.moreira.41160@gmail.com>
2023-03-24 16:12:30 +0100

    ajout de caseInteligente

---
+++ src/CaseIntelligente.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class CaseIntelligente extends Case{
+    public List<Case> lesVoisines;
+
+    public CaseIntelligente(){
+        super();
+    }
+    public void ajouteVoisine(Case uneCase) {
+        
+    }
+    private int nombreBombesVoisines() {
+        return 0;
+    }
+    @Override
+    public String toString() {
+        return "a"
+    }
+}
